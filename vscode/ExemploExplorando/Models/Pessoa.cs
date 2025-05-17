28e9852012c9f79a49a89d28034daa08ca035bdc
Marcos Alfredo Saffran <marcossaffran@gmail.com>
2025-05-17 10:21:45 -0300

    Ajustes para início do novo módulo

---
+++ vscode/ExemploExplorando/Models/Pessoa.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vscode.ExemploExplorando.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public void apresentar()
+        {
+            Console.WriteLine($"Nome: {Nome}, Idade: {Idade}");
+        }
+    }
+}
