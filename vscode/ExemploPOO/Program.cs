40d1bc711fed092d3cfe3cc0623ca5ed669fe9f8
Marcos Alfredo Saffran <marcossaffran@gmail.com>
2025-05-20 11:03:55 -0300

    Alterações ao acompanhar o curso 'Herança e Polimorfismo com C#' do módulo 'Programação Orientada a objetos com C#'

--- vscode/ExemploPOO/Program.cs
+++ vscode/ExemploPOO/Program.cs
@@ -1,10 +1,38 @@
 ﻿using ExemploPOO.Models;
 
-ContaCorrente conta1 = new ContaCorrente(12345, 1000);
-conta1.ExibirSaldo();
-conta1.Sacar(5100);
 
-conta1.ExibirSaldo();
+//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+// Polimorfismo
+Aluno aluno1 = new Aluno();
+aluno1.Nome = "Maria";
+aluno1.Idade = 20;
+aluno1.Email = "teste@teste.com";
+aluno1.Nota = 10;
+aluno1.Apresentar();
+
+Professor professor1 = new Professor();
+professor1.Nome = "João";
+professor1.Idade = 30;
+professor1.Salario = 50000;
+professor1.Apresentar();
+
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+// Herança
+
+// Aluno aluno1 = new Aluno();
+// aluno1.Nome = "Maria";
+// aluno1.Idade = 20;
+// aluno1.Email = "teste@teste.com";
+// aluno1.Apresentar();
+
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+// ContaCorrente conta1 = new ContaCorrente(12345, 1000);
+// conta1.ExibirSaldo();
+// conta1.Sacar(5100);
+
+// conta1.ExibirSaldo();
 
 
 // Pessoa pessoa1 = new Pessoa();
